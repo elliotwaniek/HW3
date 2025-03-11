c131e8a7d1bd31d1d1266757ec6fb18cdda688bc
Elliot Waniek <elliot.waniek@kellogg.northwestern.edu>
2025-03-11 23:09:29 +0000

    Commit 2?

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get 'entries/new'
+  get 'entries/create'
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
+  resources "places"
+  resources "entries"
+  get("/", { :controller => "places", :action => "index" })
 end
